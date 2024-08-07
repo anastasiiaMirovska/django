ffe06c645f12d45a3a03463cc2cc255ccf11aae9
Anastasiia Mirovska <anastasiia.mirovska@gmail.com>
2024-08-07 18:26:24 +0300

    lesson 2

---
+++ first/filter.py
@@ -0,0 +1,29 @@
+from django.db.models import QuerySet
+from django.http import QueryDict
+from rest_framework.exceptions import ValidationError
+
+from first.models import CarModel
+
+
+def car_filter(query: QueryDict) -> QuerySet :
+    qs = CarModel.objects.all()
+
+    for k,v in query.items():
+        match k:
+            case "price_gt":
+                qs = qs.filter(price__gt=v)
+            case "price_lt":
+                qs = qs.filter(price__lt=v)
+            case _:
+                raise ValidationError(f"Filter {k} is not supported")
+
+    return qs
+
+
+
+
+
+
+
+
+
--- first/models.py
+++ first/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
-class CarModel(models.Model):
+from core.models import BaseModel
+
+
+class CarModel(BaseModel):
     class Meta:
         db_table = 'cars'
 
